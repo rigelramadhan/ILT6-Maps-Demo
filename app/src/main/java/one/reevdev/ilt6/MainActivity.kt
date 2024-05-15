9d4e1441351ab67a651b5d5d82e81a78c5aec1a8
Rigel Vibi Ramadhan <rigelramadhan@gmail.com>
2024-05-15 23:16:37 +0700

    Setup Maps

--- app/src/main/java/one/reevdev/ilt6/MainActivity.kt
+++ app/src/main/java/one/reevdev/ilt6/MainActivity.kt
@@ -1,11 +1,17 @@
 package one.reevdev.ilt6
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), OnMapReadyCallback {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onMapReady(maps: GoogleMap) {
+
     }
 }
